518f462b4b04bdfc83f8f8236eb03faff04d1ae3
captainpd <93906313+captainpd@users.noreply.github.com>
2024-01-06 16:57:18 +0800

    revert

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 def square_power(a, b):
+    '''
+    It has been a while since we comment properly
+    We should try to do that for each definition :)
+    I Agree
+    '''
     result = (a ** 2 + b ** 2) ** 0.5
     return result
 
 
 
 if __name__ == "__main__":
-    c = square_power(1, 2)
+    # Add comment here
+    c = square_power(1, 3)
     print(c)
-    

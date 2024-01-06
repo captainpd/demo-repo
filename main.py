4c40601191859fe52d3df47018ac18f5ff858e9d
pd <1508732371@qq.com>
2024-01-06 16:55:08 +0800

    local test

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 def square_power(a, b):
-    '''
-    It has been a while since we comment properly
-    We should try to do that for each definition :)
-    I Agree
-    '''
     result = (a ** 2 + b ** 2) ** 0.5
     return result
 
 
 
 if __name__ == "__main__":
-    # Add comment here
-    c = square_power(1, 3)
+    c = square_power(1, 2)
     print(c)
-
+    
